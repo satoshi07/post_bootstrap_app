f86f8b556345e7dc993ddd78259d3de369238ee9
satoshi07 <satoshi.akaike7@gmail.com>
2021-05-16 00:36:09 +0900

    新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,17 @@
 class PostsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to post
   end
 
-  def edit
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 
-  def update
-  end
-
-  def destroy
-  end
 end
