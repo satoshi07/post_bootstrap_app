aea0d455bfaa3110a758f8dc74696bec377d058f
satoshi07 <satoshi.akaike7@gmail.com>
2021-05-17 22:25:53 +0900

    エラーメッセージを日本語化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: %i[show edi† update destroy]
   def index
     @posts = Post.order(id: :asc)
   end
@@ -13,22 +15,21 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
   end
 
   def destroy
-    @post = Post.find(params[:id])
-    post.destroy!
+    # @post = Post.find(params[:id])
+    @post.destroy!
     redirect_to root_path, alert: "削除しました"
   end
 
   def edit
-    @post = Post.find(params[:id])
-    post.update!(post_params)
-    redirect_to post
+    # @post = Post.find(params[:id])
   end
 
   def update
+    # @post = Post.find(params[:id])
     @post.update!(post_params)
     redirect_to @post, notice: "更新しました"
   end
